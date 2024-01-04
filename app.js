e0564f05b3cb7082bd518e1257ca14af41307b60
Krishnendu Bhunia <107338444+KB1998BH@users.noreply.github.com>
2024-01-04 23:32:00 +0530

    LEC-15, Starting with Mysql
    Watch video 5 Install the MySQL database code. Set the root user password.
    Also install the workbench. If stuck check youtube videos too and try writing the error into google and finding the answer. (If nothing work try reinstalling)
    Watch video 6 to connect to MySQL database to node js server.

--- app.js
+++ app.js
@@ -1,91 +1,10 @@
-
-//////---------project-group chat app
-// const express = require('express');
-// const fs = require("fs");
-// const bodyparser = require('body-parser');
-// const app = express();
-
-
-// app.use(bodyparser.urlencoded());
-
-// app.get("/", (req, res) => {
-//   fs.readFile('username.txt', (err, data) => {
-//     if(err){
-//       console.log(err);
-//       data = 'No chat exists'
-//     }
-//   res.send(
-//     `${data}<form action="/" method="POST" onSubmit="document.getElementById('username').value = localstorage.getItem('username')">
-//       <input type="text" name="message" id="message">
-//       <input type="hidden" name="usrname" id="username">
-//       <br/>
-//       <button type="submit">send</button>
-//     </form>
-//     `
-//   );
-//   });
-// });
-
-// app.post("/", (req, res) => {
-//   console.log(req.body.username);
-//   console.log(req.body.message);
-
-//   fs.writeFile("username.txt", `${req.body.username}:${req.body.message}`,{flag:'a'},(err) =>
-//        err ? console.log(err) : res.redirect("/")
-//   );
-// })
-
-// app.get("/login", (req, res) => {
-  
-//     res.send(
-//       `${data}<form action="/login" method="POST" onSubmit="localstorage.setItem('username', document.getElementById('username').value)">
-//         <input type="text" name="username" id="username">
-//         <input type="hidden" name="usrname" id="username">
-//         <br/>
-//         <button type="submit">send</button>
-//       </form>
-//       `
-//     );
-  
-// });
-
-// app.listen(7000)
-
-
-
-
-
-
-// const path = require('path');
-// const express = require('express');
-// const bodyparser = require('body-parser');
-// const app = express();
-// const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
-// const contactus = require('./routes/contactus');
-// const success = require('./routes/success');
-
-// app.use(bodyparser.urlencoded({extended: false}));
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/admin',adminRoutes);
-// app.use(shopRoutes);
-// app.use(contactus)
-// app.use(success);
-
-// app.use((req, res, next) => {
-//   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-// })
-// app.listen(2001)
-
-
-
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const errorcontroller = require('./controllers/error')
+const errorController = require('./controllers/error');
+const db = require('./util/database')
 
 const app = express();
 
@@ -94,17 +13,15 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const contactusRoutes = require('./routes/contactus');
-const successRoutes = require('./routes/success')
+
+db.execute('SELECT * FROM products');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(contactusRoutes);
-app.use(successRoutes)
 
-app.use(errorcontroller.get404);
+app.use(errorController.get404);
 
-app.listen(5000);
+app.listen(2000);
