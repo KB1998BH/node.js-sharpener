851fdd08882531319b5d752e5352af4d1cfe5764
Krishnendu Bhunia <107338444+KB1998BH@users.noreply.github.com>
2024-05-01 17:18:10 +0530

    Add files via upload
    Task-12:- Final Leaderboard Optimisation

---
+++ expense.js
@@ -0,0 +1,54 @@
+const Expense = require('../models/expense');
+const User = require('../models/user');
+
+const addexpense = (req, res) => {
+    const {expenseamount, description, category} = req.body;
+    if(expenseamount == undefined || expenseamount.length === 0){
+        return res.status(400).json({success:false, message: 'parameter is missing'})
+    }
+    Expense.create({expenseamount, description, category, userId:req.user.id}).then(expense => {
+        const totalExpense = Number(req.user.totalExpenses) + Number(expenseamount)
+        console.log(totalExpense);
+        User.update({
+            totalExpenses: totalExpense
+        },{
+            where: {id:req.user.id}
+        }).then(async() => {
+            res.status(200).json({expense:expense})
+        }).catch(async(err) => {
+            return res.status(500).json({success:false, error:err});
+        })
+        
+    }).catch(err => {
+        return res.status(500).json({success:false, error:err});
+    })
+}
+
+const getexpenses = (req, res) => {
+      Expense.findAll( { where: {userId: req.user.id}}).then(expenses => {
+        return res.status(200).json({expenses, success:true});
+      })
+    .catch(err =>{
+        return res.status(500).json({success: false, error: err})
+    })
+    
+}
+
+const deleteexpense = (req, res) => {
+    const expenseid = req.params.expenseid;
+    if(expenseid == undefined || expenseid.length == 0){
+        res.status(400).json({success:false,})
+    }
+    Expense.destroy({where: { id: expenseid, userId:req.user.id}}).then(() => {
+        return res.status(200).json({success:true, message: "Deleted Successfully"});
+    }).catch(err => {
+        console.log(err);
+        return res.status(500).json({success:true, message: "failed"})
+    })
+}
+
+module.exports={
+    addexpense,
+    getexpenses,
+    deleteexpense
+}
