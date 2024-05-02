75d49fcde92d3a6ca4a6248a6675d0d4b5e6048a
Krishnendu Bhunia <107338444+KB1998BH@users.noreply.github.com>
2024-05-02 09:19:05 +0530

    Add files via upload
    Task-13:- Making expense app production ready

--- expense.js
+++ expense.js
@@ -1,28 +1,34 @@
 const Expense = require('../models/expense');
 const User = require('../models/user');
+const sequelize = require('../util/database')
 
-const addexpense = (req, res) => {
-    const {expenseamount, description, category} = req.body;
-    if(expenseamount == undefined || expenseamount.length === 0){
-        return res.status(400).json({success:false, message: 'parameter is missing'})
-    }
-    Expense.create({expenseamount, description, category, userId:req.user.id}).then(expense => {
-        const totalExpense = Number(req.user.totalExpenses) + Number(expenseamount)
+
+
+
+const addexpense = async (req, res) => {
+    const t = await sequelize.transaction();
+    const { expenseamount, description, category } = req.body;
+
+    try {
+        if (expenseamount == undefined || expenseamount.length === 0) {
+            return res.status(400).json({ success: false, message: 'parameter is missing' });
+        }
+
+        const expense = await Expense.create({ expenseamount, description, category, userId: req.user.id }, { transaction: t });
+        const totalExpense = Number(req.user.totalExpenses) + Number(expenseamount);
+
         console.log(totalExpense);
-        User.update({
-            totalExpenses: totalExpense
-        },{
-            where: {id:req.user.id}
-        }).then(async() => {
-            res.status(200).json({expense:expense})
-        }).catch(async(err) => {
-            return res.status(500).json({success:false, error:err});
-        })
-        
-    }).catch(err => {
-        return res.status(500).json({success:false, error:err});
-    })
-}
+
+        await User.update({ totalExpenses: totalExpense }, { where: { id: req.user.id }, transaction: t });
+        await t.commit();
+
+        res.status(200).json({ expense });
+    } catch (err) {
+        await t.rollback();
+        return res.status(500).json({ success: false, error: err });
+    }
+};
+
 
 const getexpenses = (req, res) => {
       Expense.findAll( { where: {userId: req.user.id}}).then(expenses => {
@@ -33,6 +39,8 @@ const getexpenses = (req, res) => {
     })
     
 }
+
+
 
 const deleteexpense = (req, res) => {
     const expenseid = req.params.expenseid;
@@ -46,6 +54,7 @@ const deleteexpense = (req, res) => {
         return res.status(500).json({success:true, message: "failed"})
     })
 }
+
 
 module.exports={
     addexpense,
